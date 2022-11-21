af2e0b4a9796b9f08ac9d1bb98ba1b7b11da5500
pathbreak <5163416+pathbreak@users.noreply.github.com>
2022-11-21 08:49:48 +0530

    fix char_cnn not found error

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='hedwig',
       version='1.0.0',
       description='PyTorch deep learning models for document classification',
-      packages=['char_cnn', 'han', 'kim_cnn', 'reg_lstm', 'xml_cnn'],
-      )
+      packages=find_packages(),
+)
